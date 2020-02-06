0b4bb17e1835721c8bf93bf06996f1e33c55d4d3
qsaleh <qmsaleh@gmail.com>
2020-02-06 19:26:50 +0000

    refactorred code to include index.js file

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,16 +1,8 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
-
 
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const pool = require('./index');
 /// Users
 
 /**
